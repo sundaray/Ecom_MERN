05c21f51c801e5849fce87cf116e8ea5134260ab
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2020-11-19 13:56:28 +0530

    Adding colors to the console

--- backend/server.js
+++ backend/server.js
@@ -29,5 +29,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
