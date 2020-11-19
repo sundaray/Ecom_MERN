0297452ef5d99a805b6399a05baaa9031877125b
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2020-11-19 13:36:28 +0530

    Connecting tothe database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import products from "./data/products.js";
 
 dotenv.config();
+
+connectDB();
+
 const app = express();
 
 app.use(cors());
