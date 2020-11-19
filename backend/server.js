ddcccb6cc3339f7908ed7c414fb6e6a88740f151
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2020-11-19 10:59:57 +0530

    ES modules in node

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const products = require("./data/products");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
 dotenv.config();
 const app = express();
