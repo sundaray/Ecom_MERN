c3949bf2fa324e8544d356aa5cb853ab87a6e16d
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2020-11-20 12:30:37 +0530

    Create a Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import store from "./store";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
