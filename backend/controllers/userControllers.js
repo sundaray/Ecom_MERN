006e1686aa13cfc4e1a2c25781b9d41f10a203ee
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2020-11-28 19:09:05 +0530

    Custom authntication middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -22,3 +22,21 @@ export const authUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid email or password");
   }
 });
+
+//@desc    GET user profile
+//@route   GET api/users/profile
+//@access  Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not Found");
+  }
+});
